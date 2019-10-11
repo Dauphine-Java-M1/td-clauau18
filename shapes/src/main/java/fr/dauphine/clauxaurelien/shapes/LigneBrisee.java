e276f425f2aa703333e465c158f06d866bdcf6ca
Claux Aurelien <aurelien.claux@dauphine.eu>
2019-10-11 15:16:40 +0200

    LigneBrisee added

---
+++ shapes/src/main/java/fr/dauphine/clauxaurelien/shapes/LigneBrisee.java
@@ -0,0 +1,11 @@
+package fr.dauphine.clauxaurelien.shapes;
+
+public class LigneBrisee {
+	private Point[] points;
+	
+	public LigneBrisee(int taille)
+	{
+		this.points = new Point[taille];
+	}
+	
+}
